9b0f30ac15a03b144cc3b042fc9ab7348d21526a
Alan Ramirez <alanramirez@MacBook-Pro-de-Alan.local>
2018-08-31 08:52:17 -0500

    Second commit

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ puts "***Before exchange***"
 puts "x -> #{x}"
 puts "y -> #{y}"
 
+#Exchange without temporary variable
 x = x+y  #x -> 30 , y -> 20
 y = x - y  #x -> 30 , y -> 10
 x = x-y  #x -> 20 , y -> 10
