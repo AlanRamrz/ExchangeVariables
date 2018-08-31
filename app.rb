c4f1ab247e2977222c6d3e6510c3edfe36c5d7eb
Alan Ramirez <alanramirez@MacBook-Pro-de-Alan.local>
2018-08-31 08:50:19 -0500

    Initial commit

---
+++ app.rb
@@ -0,0 +1,14 @@
+x = 10
+y = 20
+
+puts "***Before exchange***"
+puts "x -> #{x}"
+puts "y -> #{y}"
+
+x = x+y  #x -> 30 , y -> 20
+y = x - y  #x -> 30 , y -> 10
+x = x-y  #x -> 20 , y -> 10
+
+puts "***After exchange***"
+puts "x -> #{x}"
+puts "y -> #{y}"
